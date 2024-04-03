6c01953355aa617834851305cbb24b4b3cc27fb3
Sungyong Cho <dev@sungyongcho.com>
2024-04-03 23:43:22 +0200

    feat: front app working, layout fix need

--- code/token/scripts/deployFaucet.ts
+++ code/token/scripts/deployFaucet.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import { Faucet, Faucet__factory } from "../typechain-types";
 
 async function main(): Promise<void> {
-  const TOKEN_VAL = "" // insert token val here
+  const TOKEN_VAL = "0xb632352a1b458c5a455EA026b4304C94A7a12c26" // insert token address here
   const Faucet = await ethers.getContractFactory("Faucet");
   const faucet = await Faucet.deploy(TOKEN_VAL);
 
